a7c89ea692ebb3a124550c787e0095d988fdd7b5
Ashvin Kasture <kashvin525@gmail.com>
2021-08-27 11:53:05 +0530

    modified client.py

--- client.py
+++ client.py
@@ -4,18 +4,12 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 1024
 
-client.connect((socket.gethostname(), port))
+client.connect(('192.168.29.184', port))
 
-message = 'Hello from client'
+message = 'audio'
 
 encodedMessage = message.encode('utf-8')
 
 client.send(encodedMessage)
-
-for i in range(10):
-    ip = input()
-    if ip == 'exit':
-        break
-    client.send(ip.encode('utf-8'))
 
 client.close()
